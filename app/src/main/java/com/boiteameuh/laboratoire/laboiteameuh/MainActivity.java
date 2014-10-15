12972b24f2130206175bdc3a56e6e232be2940e6
Clément Godart <clement.godart@y-nov.com>
2014-10-15 17:11:41 +0200

    Ajout rire christophe lambert

--- app/src/main/java/com/boiteameuh/laboratoire/laboiteameuh/MainActivity.java
+++ app/src/main/java/com/boiteameuh/laboratoire/laboiteameuh/MainActivity.java
@@ -1,29 +1,46 @@
 package com.boiteameuh.laboratoire.laboiteameuh;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
 
 public class MainActivity extends Activity {
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        /* Bouton de test pour lancer le son*/
-        Button ButtonTest = (Button) findViewById(R.id.main_button_test);
-        ButtonTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
+        Button btnLecture = (Button)this.findViewById(R.id.btn_lecture);
+        btnLecture.setOnClickListener(
+                new OnClickListener(){
+                    public void onClick(View viewParam) {
+                        mediaPlayer =
+                                MediaPlayer.create(MainActivity.this, R.raw.lambert);
+// Planifier une action à la fin de la lecture du fichier.
+                        mediaPlayer.setOnCompletionListener(new OnCompletionListener(){
+                            public void onCompletion(MediaPlayer mediaPlayerParam) {
+// Reste à coder l’action à réaliser.
+                            }
+                        });
+                        mediaPlayer.start();
+                    }
+                });
     }
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+// Libération des ressources si nécessaire
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
 }
